1ca03cad5bc1680299b2f3f171379e85ad73c4c6
Jennifer Wang <1155136569@link.cuhk.edu.hk>
2023-11-12 20:32:51 -0500

    added pseudocode

--- main.cpp
+++ main.cpp
@@ -18,43 +18,75 @@ const int HEIGHT = 900;               // default window height
 
 class Player {
 private:
+  int health;
 
 public:
   Player() {}
   ~Player() {}
 
   void update() {
+    // update player movement
+
+    // update player status (health)
   }
 
   void draw() {
+    // draw player object
   }
 };
 
 class Enemy {
 private:
+  int positionX;
+  int positionY;
+  int speed;
+  int health;
+  int damage;
 
 public:
   Enemy() {}
   ~Enemy() {}
 
-  void update() {
+  void update() {   
+    // update enemy movement
+
+    // update enemy state (health, damage)
   }
 
   void draw() {
+    // draw enemy object
   }
 };
 
 class Game {
 private:
+  int score;
+  int time;
+  // You might want to use a data structure for storing enemies, e.g., std::vector<Enemy> enemies;
+  // Consider adding other necessary attributes for your game
+
+  void displaySplashScreen() {
+      // Implementation for displaying the splash screen transition
+  }
+
+  void handleInput() {
+      // Implementation for handling user input
+  }
+
 
 public:
   Game() {}
   ~Game() {}
 
   void update() {
+    // Update game state
+    handleInput();  // Check for user input
+    // Update score, time, enemies, and other game elements
   }
 
   void draw() {
+    // Draw game elements
+    // Draw score, time, enemies, and other game elements
   }
 };
 
